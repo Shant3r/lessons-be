195b5689ad23018f590636e3addb25610e95ec57
Shant3r <shanter@yandex.ru>
2022-10-24 00:39:12 +0300

    1

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("32132 × 42452 =", 32132*42452)
+    for i := 1; i <= 10; i++ {
+       if i % 2 == 0 {
+            fmt.Println(i, "even")
+        } else {
+            fmt.Println(i, "odd")
+        }
+    }
 }
